6ff4e4f8e38b3f06672126b7e03a3b8db8657fe9
Marknjo <marknjo7@gmail.com>
2023-05-18 17:30:59 +0300

    fix(main): offscreen rendering not listening on did-finish-load event

--- src/main/get-page-meta.js
+++ src/main/get-page-meta.js
@@ -6,6 +6,24 @@ let offscreenWindow;
 app.disableHardwareAcceleration();
 
 /**
+ *
+ * @param {BrowserWindow} win
+ */
+const captureLoadedPageImage = (win, cb, url) => {
+  const title = win.getTitle();
+
+  win.webContents.capturePage().then((image) => {
+    let screenshot = image.toDataURL();
+
+    /// Resolve success
+    cb({ data: { title, screenshot, url }, status: "success" });
+
+    win.close();
+    win = null;
+  });
+};
+
+/**
  * Get's a page metadata given url
  * @param {string} url
  * @param {function} callback
@@ -23,18 +41,19 @@ module.exports = async (url, callback) => {
 
     await offscreenWindow.loadURL(url);
 
+    let isCaptured = false;
+
+    /// capture the page when window has finished loading
     offscreenWindow.webContents.on("did-finish-load", () => {
-      let title = offscreenWindow.getTitle();
+      isCaptured = true;
+      captureLoadedPageImage(offscreenWindow, callback, url);
+    });
 
-      offscreenWindow.webContents.capturePage().then((image) => {
-        let screenshot = image.toDataURL();
-
-        /// Resolve success
-        callback({ data: { title, screenshot, url }, status: "success" });
-
-        offscreenWindow.close();
-        offscreenWindow = null;
-      });
+    /// wait 1.5 secs to capture a page if did-finish events is not called
+    setImmediate(() => {
+      setTimeout(() => {
+        isCaptured || captureLoadedPageImage(offscreenWindow, callback, url);
+      }, 1500);
     });
   } catch (error) {
     /// Resolve with error
